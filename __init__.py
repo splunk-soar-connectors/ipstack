6181eaec10f3a878db2a1e880ad00478a5646462
Trent Beals <trent.beals@phantom.us>
2018-07-02 10:35:31 -0700

    PAPP-4541 #resolve #comment adding copyright line back in to header

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 # --
 # File: __init__.py
 #
+# Copyright (c) Splunk Inc., 2018-2019
+#
 # SPLUNK CONFIDENTIAL - Use or disclosure of this material in whole or in part
 # without a valid written license from Splunk Inc. is PROHIBITED.
 #
